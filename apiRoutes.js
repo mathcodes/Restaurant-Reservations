5f27a82e25905b10de140f2b178490a087c0fd8c
Macy <59153195+Macymo@users.noreply.github.com>
2020-05-16 15:10:00 -0400

    submitting working code

--- apiRoutes.js
+++ apiRoutes.js
@@ -4,8 +4,8 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-const tableData = require("./tableData");
-const waitListData = require("./waitingListData");
+var tableData = require("./tableData");
+var waitListData = require("./waitingListData");
 
 
 // ===============================================================================
@@ -20,11 +20,11 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("/api/tables", function(req, res) {
-    res.sendFile(tableData);
+    res.json(tableData);
   });
 
   app.get("/api/waitlist", function(req, res) {
-    res.sendFile(waitListData);
+    res.json(waitListData);
   });
 
   // API POST Requests
@@ -35,35 +35,29 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-    app.post("/api/tables", function(req, res) {
-    var newReservation = req.body;
-        if (tableData.length < 5) {
-        tableData.push(newReservation);
-        }
-        console.log(newReservation);
-        res.json(newReservation);
-    });
+  app.post("/api/tables", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    if (tableData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    }
+    else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
 
-    app.post("/api/tables", function(req, res) {
-        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-        // It will do this by sending out the value "true" have a table
-        // req.body is available since we're using the body parsing middleware
-        if (tableData.length < 5) {
-            tableData.push(req.body);
-            res.json(true);
-        } else {
-            waitListData.push(req.body);
-            res.json(false);
-        }
-    });
-    // ---------------------------------------------------------------------------
-    // I added this below code so you could clear out the table while working with the functionality.
-    // Don"t worry about it!
-    app.post("/api/clear", function(req, res) {
-        // Empty out the arrays of data
-        tableData.length = 0;
-        waitListData.length = 0;
-        res.json({ ok: true });
-    });
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    tableData.length = 0;
+    waitListData.length = 0;
+
+    res.json({ ok: true });
+  });
 };
-
