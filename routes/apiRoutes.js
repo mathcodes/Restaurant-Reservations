bfb2bda6f2dc6d200dbcefb7cf1fb2a536320ed7
AFeese <58886725+AFeese@users.noreply.github.com>
2020-05-16 13:09:34 -0400

    pushing folder, will need to extract specific files needed.

---
+++ routes/apiRoutes.js
@@ -0,0 +1,56 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+const tableData = require("../data/tableData");
+
+const waitListData = require("../data/waitinglistData");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/tables", function(req, res) {
+    res.sendFile(tableData);
+  });
+
+  app.get("/api/waitlist", function(req, res) {
+    res.sendFile(waitListData);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/tables", function(req, res) {
+    var newReservation = req.body;
+    if (tableData.length < 5) {
+      tableData.push(newReservation);
+    }
+
+    console.log(newReservation);
+  });
+
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    tableData.length = 0;
+    waitListData.length = 0;
+    res.json({ ok: true });
+  });
+};
+
+
